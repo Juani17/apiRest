364a673f6edb2fc9e2e82c45292ae147fd3eb494
Juani17 <juanifrery@gmail.com>
2025-05-18 15:17:52 -0300

    termino de subir algunas cosas

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import usuarioRoutes from './routes/usuarioRoutes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/usuarios', usuarioRoutes);
+
+export { app }; // 👈 ¡esto es lo importante!
